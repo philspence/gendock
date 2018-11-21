71dc73ea8f7ea9290021f1542b7814c518edb688
philspence <philspence91@gmail.com>
2018-11-21 00:17:52 +0000

    predict.py is now working at a basic level

--- scripts/predict.py
+++ scripts/predict.py
@@ -1,20 +1,29 @@
 from rdkit import Chem, DataStructs
 from rdkit.Chem import AllChem
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 import csv
-import pandas as pd
 
 #def learn(input):
 input = "NCIDiv4-fpocket_results.csv"
+
+
+testfp = []
+testfp.append(AllChem.GetMorganFingerprintAsBitVect(Chem.MolFromSmiles("c1ccccc1"), 2))
+testmol = []
+for fp in testfp:
+    arr = np.zeros((1,))
+    DataStructs.ConvertToNumpyArray(fp, arr)
+    testmol.append(arr)
 
 header = []
 rawdata = []
 infile = open(input, "r")
 reader = csv.reader(infile, delimiter=',')
 rownum = 0
+fps = []
+nrgs = []
 for row in reader:
-    temp = []
     if rownum == 0:
         pass
     else:
@@ -23,16 +32,20 @@ for row in reader:
         else:
             mol = Chem.MolFromSmiles(row[3])
             fp = AllChem.GetMorganFingerprintAsBitVect(mol, 2)
-            #temp.append(mol)
-            temp.append(fp)
-            temp.append(float(row[4]))
-            rawdata.append(temp)
+            fps.append(fp)
+            nrgs.append(float(row[4]))
     rownum += 1
 infile.close()
 
-
-# data = pd.DataFrame({
-#     'fingerprint':rawdata[:,0],
-#     'energy':rawdata[:,1]
-# })
-# data.head()
+np_fps = []
+rf = RandomForestRegressor(n_estimators=1000, n_jobs=2)
+for fp in fps:
+    arr = np.zeros((1,))
+    DataStructs.ConvertToNumpyArray(fp, arr)
+    np_fps.append(arr)
+print('Fitting data...')
+rf.fit(np_fps, nrgs)
+print('Calculating prediction...')
+nrg_pred = rf.predict(testmol)
+print(nrg_pred)
+print('Done.')
